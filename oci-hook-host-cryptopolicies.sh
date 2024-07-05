624d24e01be5db7dd2772270aaa3a8c5ab358f50
Fabian Deutsch <fabiand@fedoraproject.org>
2024-07-05 10:14:45 +0200

    Initial commit
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

---
+++ oci-hook-host-cryptopolicies.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+echo "Running $0"
+
+set -x
+
+CG_PATH=$(jq -er '.linux.cgroupsPath' < config.json)
+POD_NAME=$(jq -er '.annotations["io.kubernetes.pod.name"]' < config.json)
+
+if [[ "$CG_PATH" =~ .*"burst".* && "$POD_NAME" =~ "virt-launcher".* ]];
+then
+  CONTAINERID=$(jq -er '.linux.cgroupsPath | split(":")[2]' < config.json)
+  runc update $CONTAINERID --memory-swap -1
+fi
+
+
+
