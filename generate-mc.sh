9d1203e39f426fc9caf48605d6a870d139af57b2
Fabian Deutsch <fabiand@fedoraproject.org>
2024-07-05 12:01:59 +0200

    Fix indentation
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- generate-mc.sh
+++ generate-mc.sh
@@ -4,18 +4,18 @@ cat <<EOY
 apiVersion: machineconfiguration.openshift.io/v1
 kind: MachineConfig
 metadata:
-labels:
-  machineconfiguration.openshift.io/role: worker
-name: 50-install-oci-hook-cryptopolicies
+  labels:
+    machineconfiguration.openshift.io/role: worker
+  name: 50-install-oci-hook-cryptopolicies
 spec:
-config:
-  ignition:
-    version: 3.1.0
-  storage:
-    files:
-      - path: "/run/containers/oci/hooks.d/oci-hook-host-cryptopolicies.json"
-        contents:
-          source: "data:text/plain;charset=utf-8;base64,$(base64 -w 0 contrib/hook.json)"
-        mode: 644
-        overwrite: true
+  config:
+    ignition:
+      version: 3.1.0
+    storage:
+      files:
+        - path: "/run/containers/oci/hooks.d/oci-hook-host-cryptopolicies.json"
+          contents:
+            source: "data:text/plain;charset=utf-8;base64,$(base64 -w 0 contrib/hook.json)"
+          mode: 644
+          overwrite: true
 EOY
